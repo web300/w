62221f118c076f8d6ac7be49893fc6743aa6109f
web300 <315148895@qq.com>
2020-12-24 22:41:20 +0800

    Add files via upload

---
+++ index/ti.js
@@ -0,0 +1,6 @@
+var zbpConfig = {
+    bloghost: "https://www.179885.com/",
+};
+var zbp = new ZBP(zbpConfig);
+
+document.writeln("<script src='//cdn.jsdelivr.net/gh/web300/w/index/prism.js' type='text/javascript'></script><link rel='stylesheet' type='text/css' href='//cdn.jsdelivr.net/gh/web300/w/index/prism.css'/>");$(function(){var compatibility={as3:"actionscript","c#":"csharp",delphi:"pascal",html:"markup",xml:"markup",vb:"basic",js:"javascript",plain:"markdown",pl:"perl",ps:"powershell"};var runFunction=function(doms,callback){doms.each(function(index,unwrappedDom){var dom=$(unwrappedDom);var codeDom=$("<code>");if(callback)callback(dom);var languageClass="prism-language-"+function(classObject){if(classObject===null)return"markdown";var className=classObject[1];return compatibility[className]?compatibility[className]:className}(dom.attr("class").match(/prism-language-([0-9a-zA-Z]+)/));codeDom.html(dom.html()).addClass("prism-line-numbers").addClass(languageClass);dom.html("").addClass(languageClass).append(codeDom)})};runFunction($("pre.prism-highlight"));runFunction($('pre[class*="brush:"]'),function(preDom){var original;if((original=preDom.attr("class").match(/brush:([a-zA-Z0-9\#]+);/))!==null){preDom.get(0).className="prism-highlight prism-language-"+original[1]}});Prism.highlightAll()});
